592fd0f203a218eb2f4d94c46d4e84668f51f447
Ben Jackson <Ben@JacksonBR.com>
2021-12-07 22:17:48 +0000

    Day two - Dive Part two

--- aoc/dive.py
+++ aoc/dive.py
@@ -20,16 +20,42 @@ def dive(data, start=[0, 0]):
     return pos
 
 
-def forward(pos, value):
-    forward_mat = [value, 0]
-    return list(map(add, pos, forward_mat))
+def adv_dive(data, start=[0, 0], aim=0):
+    pos = start
+    aim = aim
+    for d in data:
+        inst, value = d.split()
+        value = int(value)
+        logging.debug(f"INST: {inst} \tVAL: {value}")
+        if inst == "forward":
+            pos = forward(pos, value, aim)
+        elif inst == "down":
+            aim = aim_down(aim, value)
+        elif inst == "up":
+            aim = aim_up(aim, value)
+    logging.debug(pos)
+    return pos
+
+
+def forward(pos, value, aim=0):
+    forward_mat = [value, value*aim]
+    logging.debug(forward_mat)
+    return [sum(x) for x in zip(pos, forward_mat)]
 
 
 def down(pos, value):
     down_mat = [0, value]
-    return list(map(add, pos, down_mat))
+    return [sum(x) for x in zip(pos, down_mat)]
+
+
+def aim_down(aim, value):
+    return aim + value
 
 
 def up(pos, value):
     up_mat = [0, -value]
-    return list(map(add, pos, up_mat))
+    return [sum(x) for x in zip(pos, up_mat)]
+
+
+def aim_up(aim, value):
+    return aim - value
--- test/test_dive.py
+++ test/test_dive.py
@@ -20,3 +20,23 @@ class TestDive(unittest.TestCase):
         result = pos[0] * pos[1]
         print(result)
         self.assertEqual(1855814, result)
+
+
+class TestAdvancedDive(unittest.TestCase):
+    def test_example(self):
+        data = ""
+        with open("./data/dive_e.dat") as raw_data:
+            data = raw_data.readlines()
+        pos = adv_dive(data)
+        result = pos[0] * pos[1]
+        print(pos, result)
+        self.assertEqual(900, result)
+
+    def test_stage_1(self):
+        data = ""
+        with open("./data/dive_1.dat") as raw_data:
+            data = raw_data.readlines()
+        pos = adv_dive(data)
+        result = pos[0] * pos[1]
+        print(result)
+        self.assertEqual(1845455714, result)
